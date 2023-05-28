9dbdf567e14f8b2c0ad06751d315361508d8d683
Faruuq <faruuqq@icloud.com>
2023-05-28 11:38:01 +0700

    Initial Commit

---
+++ MovieAppHome/MovieAppHome.h
@@ -0,0 +1,18 @@
+//
+//  MovieAppHome.h
+//  MovieAppHome
+//
+//  Created by Muhammad Faruuq Qayyum on 28/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieAppHome.
+FOUNDATION_EXPORT double MovieAppHomeVersionNumber;
+
+//! Project version string for MovieAppHome.
+FOUNDATION_EXPORT const unsigned char MovieAppHomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieAppHome/PublicHeader.h>
+
+
